4ed17f311f4da5287fbe033fe3c7e87191ada4e3
handyman0 <luiz.number@gmail.com>
2025-06-26 20:39:23 -0300

    add: implement ft_strlcpy function for string copying with size limit

---
+++ ft_strlcpy.c
@@ -0,0 +1,30 @@
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if (!size)
+		return (ft_strlen(src));
+	while (src[i] && i < size - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (ft_strlen(src));
+}
+
+/* int	main(void)
+{
+	char dest[20] = "Hello";
+	char src[] = "fala galerinha do canal sabia que nao adianta defender politico pois todos sao ladroes";
+	size_t size = 20;
+
+	size_t result = ft_strlcpy(dest, src, size);
+	printf("Resultado: %zu\n", result);
+	printf("Destino após a cópia: %s\n", dest);
+
+	return 0;
+} */
