b6c470eb4a472ec887733f5f2220e3214db829bd
lmelo-do <lmelo-do@student.42sp.org.br>
2025-07-13 15:01:51 -0300

    add: implement ft_strchr function for character search in strings

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,2 +1,26 @@
 #include "libft.h"
 
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	return (NULL);
+}
+#include <stdio.h>
+int	main(void)
+{
+	const char	*str = "ola, mundo";
+	char	*ptr = ft_strchr(str, 'u');
+	if (ptr != NULL)
+		printf("Caractere encontrado: %c\n", *ptr);
+	else
+		printf("caractere não encontrado");
+	return (0);
+}
