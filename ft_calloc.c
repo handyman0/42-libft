f8220529e36e407dd0d7463f2eb22b2e82c68fae
lmelo-do <lmelo-do@student.42sp.org.br>
2025-07-16 17:24:24 -0300

    add: function calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,13 +6,20 @@
 /*   By: lmelo-do <lmelo-do@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 16:06:19 by lmelo-do          #+#    #+#             */
-/*   Updated: 2025/07/13 20:07:14 by lmelo-do         ###   ########.fr       */
+/*   Updated: 2025/07/16 17:13:53 by lmelo-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*calloc(size_t nmemb, size_t size)
+void	*ft_calloc(size_t nmemb, size_t size)
 {
-	
+	void	*ptr;
+
+	if (size != 0 && nmemb > (size_t)-1 / size)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr != NULL)
+		ft_memset(ptr, 0, nmemb * size);
+	return (ptr);
 }
