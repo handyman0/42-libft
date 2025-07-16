edb971185c82a3fdcf660bdfa73fbe5df1db218d
lmelo-do <lmelo-do@student.42sp.org.br>
2025-07-16 17:25:37 -0300

    add: function strtrim

---
+++ ft_strtrim.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmelo-do <lmelo-do@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/14 19:31:19 by lmelo-do          #+#    #+#             */
+/*   Updated: 2025/07/16 17:20:02 by lmelo-do         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	is_in_set(char c, char *set)
+{
+	while(*set)
+	{
+		if (c == *set)
+			return(1);
+		set++;
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	int	start;
+	int	end;
+	int	len;
+
+	start = 0;
+	while (s1[start] && is_in_set(s1[start], (char *)set))
+		start++;
+	end = ft_strlen(s1) - 1;
+	while (end >= start && is_in_set(s1[end], (char *)set))
+		end--;
+	len = end - start + 1;
+	return ft_substr(s1, start, len);
+}
