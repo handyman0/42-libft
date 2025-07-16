d3ea04f0bda855ff58c2cac87044dd31d22e9e95
lmelo-do <lmelo-do@student.42sp.org.br>
2025-07-16 17:26:22 -0300

    add: function substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,8 +6,25 @@
 /*   By: lmelo-do <lmelo-do@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 16:36:49 by lmelo-do          #+#    #+#             */
-/*   Updated: 2025/07/13 16:36:59 by lmelo-do         ###   ########.fr       */
+/*   Updated: 2025/07/14 19:26:06 by lmelo-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	real_len;
+	char	*p;
+
+	if (s ==  NULL)
+		return (NULL);
+	if (start >= ft_strlen(s))
+		return (ft_strdup(""));
+	real_len = ft_strlen(s) - start;
+	if (len > real_len)
+		len = real_len;
+	p = ft_calloc(len + 1, sizeof(char));
+	ft_memcpy(p, s + start, len);
+	return (p);
+}
