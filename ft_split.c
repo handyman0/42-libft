2f52e1327e89a558e621296e37ee3aa02b3b5a96
lmelo-do <lmelo-do@student.42sp.org.br>
2025-07-16 22:20:27 -0300

    add split

--- ft_split.c
+++ ft_split.c
@@ -6,13 +6,63 @@
 /*   By: lmelo-do <lmelo-do@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 19:36:37 by lmelo-do          #+#    #+#             */
-/*   Updated: 2025/07/16 17:20:47 by lmelo-do         ###   ########.fr       */
+/*   Updated: 2025/07/16 21:37:40 by lmelo-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	ft_word_len(const char *s, char c)
+{
+	int	i;
+
+	i = 0;
+	while(s[i] != c)
+		i++;
+	return (i);
+}
+
+int	count_words(const char *s, char c)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		if (s[i])
+			count++;
+		while (s[i] && s[i] != c)
+			i++;
+	}
+	return (count);
+}
+
 char	**ft_split(char const *s, char c)
 {
-	
+	int	i;
+	int	j;
+	int	start;
+	char **newarray;
+
+	j = 0;
+	i = 0;
+	newarray = malloc((count_words(s, c) + 1) * sizeof(char *));
+	if (!newarray)
+		return (NULL);
+	while (s[i])
+	{
+		while (s[i] == c)
+			i++;
+		start = i;
+		while (s[i] && s[i] != c)
+			i++;
+		if (i > start)
+			newarray[j++] = ft_substr(s, start, i - start);
+	}
+	newarray[j] = NULL;
+	return (newarray);
 }
