b0bb98bffaec85ad5dd1152f3e4e3351f504f40d
handyman0 <luiz.number@gmail.com>
2025-07-20 22:27:57 -0300

    cleaning code

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -9,7 +9,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	len = ft_strlen(s);
 	if (!s || !f)
 		return (NULL);
-	result = malloc(ft_strlen(s) + 1);
+	result = malloc(len + 1);
 	if (!result)
 		return (NULL);
 	i = 0;
