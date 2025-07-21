f1120d98c49e1a1e6ac027d2a6eb5d8fecdfa4d6
handyman0 <luiz.number@gmail.com>
2025-07-20 22:48:11 -0300

    add putstr_fd

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,8 +6,17 @@
 /*   By: lmelo-do <lmelo-do@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/13 16:38:54 by lmelo-do          #+#    #+#             */
-/*   Updated: 2025/07/20 22:33:01 by lmelo-do         ###   ########.fr       */
+/*   Updated: 2025/07/20 22:45:53 by lmelo-do         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	while (*s)
+	{
+		write(fd, s, 1);
+		s++;
+	}
+}
